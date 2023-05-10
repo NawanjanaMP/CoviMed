e0a2bdff5284a7e84278ed3839be7b223538f1da
Nawanjana Madhushankha Pathirage <88629690+NawanjanaMP@users.noreply.github.com>
2023-05-10 19:41:39 +0530

    Add Files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CoviMed
 Covid19 Testing Management System
+
+# How to run the COVID19 Testing Management System Using PHP and MySQL
+
+Download the zip file
+Extract the file and copy CoviMed folder
+Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+Open PHPMyAdmin (http://localhost/phpmyadmin)
+Create a database with name covidtmsdb
+Import covidtmsdb.sql file(given inside the zip package in SQL file folder)
+Run the script http://localhost/CoviMed
+
+Admin Credential
+Username: admin
+Password: Test@123
---
+++ index.php
@@ -0,0 +1,412 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CoviMed </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: SoftLand - v4.6.0
+  * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.php">CoviMed</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.php">Home</a></li>
+          <li><a href="#covid">Covid-19 Symptoms</a></li>
+          <li><a href="#prevention">Prevention</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+          <li><a href="covid-tms/new-user-testing.php">Testing</a></li>
+          <li><a href="covid-tms/live-test-updates.php">Live Updates</a></li>
+          <li><a href="covid-tms/login.php">Admin</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section class="hero-section" id="hero">
+
+    <div class="wave">
+
+      <svg width="100%" height="355px" viewBox="0 0 1920 355" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+            <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,757 L1017.15166,757 L0,757 L0,439.134243 Z" id="Path"></path>
+          </g>
+        </g>
+      </svg>
+
+    </div>
+
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 hero-text-image">
+          <div class="row">
+            <div class="col-lg-8 text-center text-lg-start">
+              <h1 data-aos="fade-right">Get Your Self Protected</h1>
+              <p class="mb-5" data-aos="fade-right" data-aos-delay="100">Test Yourself For Better Insurance.</p>
+              <p data-aos="fade-right" data-aos-delay="200" data-aos-offset="-500"><a href="#we" class="btn btn-outline-white">Get started</a></p>
+            </div>
+            <div class="col-lg-4 iphone-wrap">
+              <img src="assets/img/mobile_3.png" alt="Image" class="phone-1" data-aos="fade-right">
+              <img src="assets/img/mobile_1.png" alt="Image" class="phone-2" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Home Section ======= -->
+    <section class="section" id=we>
+      <div class="container">
+
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">We got what you need</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-thermometer-half"></i>
+              </div>
+              <h3 class="mb-3">Care</h3>
+              <p>Consult a doctor at your earliest convenience once you get infected with cold and high fever.</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-heart"></i>
+              </div>
+              <h3 class="mb-3">Life</h3>
+              <p>Life is precious so have social distance.</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-bar-chart"></i>
+              </div>
+              <h3 class="mb-3">Important</h3>
+              <p>Join us, We Know the importance of your life.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section" id="covid">
+
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5" data-aos="fade">
+          <div class="col-md-6 mb-5">
+            <img src="assets/img/symptoms.png" alt="Image" class="img-fluid">
+          </div>
+        </div>
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">Covid-19 Symptoms</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">01</span>
+              <h3>Hight Fever 2-14 days</h3>
+              <p>Reported illnesses have ranged from mild symptoms to severe illness and death.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">02</span>
+              <h3>Dry Cough 2-14 days</h3>
+              <p>Reported illnesses have ranged from mild symptoms to severe illness and death.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">03</span>
+              <h3>Shortness of breath</h3>
+              <p>Reported illnesses have ranged from mild symptoms to severe illness and death.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section" id="prevention">
+
+<div class="container">
+  <div class="row justify-content-center text-center mb-5" data-aos="fade">
+    <div class="col-md-6 mb-5">
+      <img src="assets/img/prevent.png" alt="Image" class="img-fluid">
+    </div>
+  </div>
+  <div class="row justify-content-center text-center mb-5">
+    <div class="col-md-5" data-aos="fade-up">
+      <h2 class="section-heading">Prevention</h2>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-3">
+      <div class="step">
+        <span class="number">01</span>
+        <h3>Wash your Hands at least 10 minutes</h3>
+        
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="step">
+        <span class="number">02</span>
+        <h3>Wear A Face mask when you go out</h3>
+        
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="step">
+        <span class="number">03</span>
+        <h3>Avoid contact with sick people</h3>
+        
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="step">
+        <span class="number">04</span>
+        <h3>Always cover your cough or sneeze</h3>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+</section>
+
+
+
+
+
+
+    <section class="section" id="contact">
+      <div class="container">
+      <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">Contact Us</h2>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col-md-4 ms-auto order-2">
+          <img src="assets/img/contact.png" alt="Image" class="img-fluid">
+            
+          </div>
+          
+          <?php 
+		if(isset($_POST['sendmail'])) {
+			require 'PHPMailerAutoload.php';
+			require 'credential.php';
+
+			$mail = new PHPMailer;
+
+			// $mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = EMAIL;                 // SMTP username
+			$mail->Password = PASS;                           // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+
+			$mail->setFrom(EMAIL, 'CoviMed');
+			$mail->addAddress($_POST['email']);     // Add a recipient
+			$mail->addReplyTo(EMAIL);
+			// print_r($_FILES['file']); exit;
+			/*for ($i=0; $i < count($_FILES['file']['tmp_name']) ; $i++) { 
+				$mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);    // Optional name
+			}*/
+			$mail->isHTML(true);                                  // Set email format to HTML
+
+			$mail->Subject = $_POST['subject'];
+			$mail->Body    = '<b>We will contact you soon.</b></div>';
+			$mail->AltBody = $_POST['message'];
+
+			if(!$mail->send()) {
+			    echo 'Message could not be sent.';
+			    echo 'Mailer Error: ' . $mail->ErrorInfo;
+			} else {
+                echo "<script>alert('Your Message Has Been Sent Successfully'); window.location.href='index.php';</script>";
+
+			}
+		}
+	 ?>
+          <div class="col-md-6" data-aos="fade-right">
+            <form role="form" method="post" enctype="multipart/form-data">
+
+              <div class="row">
+                <div class="col-md-6 form-group">
+               
+                  <label for="name">Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <label for="name">Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+                <div class="col-md-12 form-group mt-3">
+                  <label for="name">Subject</label>
+                  <input type="text" class="form-control" name="subject" id="subject" required>
+                </div>
+                <div class="col-md-12 form-group mt-3">
+                  <label for="name">Message</label>
+                  <textarea class="form-control" name="message" id="message" required></textarea>
+                </div>
+
+               
+
+                <div class="col-md-6 form-group"><br>
+                  <input type="submit" class="btn btn-primary d-block w-100" name="sendmail" value="Send Message">
+                </div>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+
+
+    <!-- ======= CTA Section ======= -->
+    <section class="section cta-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
+            <h2>Stay Home & Stay Safe</h2>
+          </div>
+          <div class="col-md-5 text-center text-md-end">
+            <p><a href="covid-tms/new-user-testing.php" class="btn d-inline-flex align-items-center"><i class="bi bi-file-earmark-medical"></i><span>Testing</span></a> <a href="covid-tms/live-test-updates.php" class="btn d-inline-flex align-items-center"><i class="bi bi-bar-chart"></i><span>Live Updates</span></a></p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End CTA Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <h3>About CoviMed</h3>
+          <p>We are providing direct services to patients who are wanted to check their PCR tests.</p>
+          <p class="social">
+            <a href="#"><span class="bi bi-twitter"></span></a>
+            <a href="#"><span class="bi bi-facebook"></span></a>
+            <a href="#"><span class="bi bi-instagram"></span></a>
+            <a href="#"><span class="bi bi-linkedin"></span></a>
+          </p>
+        </div>
+        <div class="col-md-7 ms-auto">
+          <div class="row site-section pt-0">
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Navigation</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Covid-19 Symptoms</a></li>
+                <li><a href="#">Prevention</a></li>
+                <li><a href="#">Contact Us</a></li>
+                
+              </ul>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Services</h3>
+              <ul class="list-unstyled">
+                <li><a href="covid-tms/new-user-testing.php">Testing</a></li>
+                <li><a href="covid-tms/live-test-updates.php">Live Updates</a></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center text-center">
+        <div class="col-md-7">
+          <p class="copyright">&copy; Copyright CoviMed. All Rights Reserved</p>
+          <div class="credits">
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=SoftLand
+          -->
+            Designed by <a href="#">Nawanjana Madhushankha</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
